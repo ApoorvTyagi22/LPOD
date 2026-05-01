70a84a726460dd836a378bc11caf189e1675a9bc
Apoorv Tyagi <tyagip872@gmail.com>
2026-05-01 11:05:26 +0100

    Fri1May

---
+++ maxRotateFunction.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxRotateFunction(int[] nums) {
+        int n = nums.length; 
+        int numSum = 0;
+        int f0 = 0;
+        for(int i = 0; i < n; i++){
+            numSum += nums[i];
+            f0 += (i * nums[i]);
+        }
+        int maxRes = f0;
+        int lastSum = f0;
+        for(int i = 1; i < n; i++){
+            int tmp = numSum + lastSum - (n * nums[n - i]);// n - i first value n - 1
+            maxRes = Math.max(maxRes, tmp);
+            lastSum = tmp;
+        }
+
+        return maxRes; 
+    }
+}
