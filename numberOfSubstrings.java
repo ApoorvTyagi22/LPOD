212e1a34fb1fb2f8d25c1ce89d675d36a30ad6f9
Apoorv Tyagi <at2u23@soton.ac.uk>
2026-06-30 23:17:38 +0100

    Tue30Jun

---
+++ numberOfSubstrings.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numberOfSubstrings(String s) {
+        int n = s.length();
+
+        int[] freq = new int[3];
+
+        int i = 0; 
+        int j = 0; 
+        int res = 0; 
+        while(j < n){
+            freq[s.charAt(j) -'a']++;
+
+            while(freq[0] > 0 && freq[1] > 0 && freq[2] > 0){
+                res += (n - j);
+                freq[s.charAt(i) - 'a']--;
+                i++;
+            }
+            j++;
+        }
+
+
+        return res; 
+    }
+}
