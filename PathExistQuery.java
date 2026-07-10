7800c43fe8d8933cad92c2aa5bf011fff0bf8c27
Apoorv Tyagi <at2u23@soton.ac.uk>
2026-07-11 00:44:54 +0100

    Fri10Jun

--- PathExistQuery.java
+++ PathExistQuery.java
@@ -1,77 +1,98 @@
 class Solution {
 
-    public int[] pathExistenceQueries(
-        int n,
-        int[] nums,
-        int maxDiff,
-        int[][] queries
-    ) {
-        int[] idx = new int[n];
-        int[] pos = new int[n];
 
-        for (int i = 0; i < n; i++) {
-            idx[i] = i;
-        }
+    int UpperBoundBinary(int[][] arr, int target){
+        int n = arr.length; 
+        int l = 0; 
+        int r = n - 1;
+        int res = 0;  // maxValue = val + maxDiff <= newVal 
 
-        Integer[] ord = new Integer[n];
-        for (int i = 0; i < n; i++) {
-            ord[i] = i;
-        }
-
-        Arrays.sort(ord, (a, b) -> Integer.compare(nums[a], nums[b]));
-
-        for (int i = 0; i < n; i++) {
-            idx[i] = ord[i];
-            pos[idx[i]] = i;
-        }
-
-        int m = 32 - Integer.numberOfLeadingZeros(n);
-
-        int[][] f = new int[n][m];
-
-        int left = 0;
-        for (int i = 0; i < n; i++) {
-            while (left < i && nums[idx[i]] - nums[idx[left]] > maxDiff) {
-                left++;
-            }
-            f[i][0] = left;
-        }
-
-        for (int j = 1; j < m; j++) {
-            for (int i = 0; i < n; i++) {
-                f[i][j] = f[f[i][j - 1]][j - 1];
+        while(l <= r){
+            int mid = l + (r - l)/2; 
+            // is the midValue 
+            if(arr[mid][0] <= target){
+                res = mid; 
+                l = mid + 1; 
+            } else {
+                r = mid - 1; 
             }
         }
 
-        int[] ans = new int[queries.length];
+        return res; 
 
-        for (int k = 0; k < queries.length; k++) {
-            int x = pos[queries[k][0]];
-            int y = pos[queries[k][1]];
 
-            if (x > y) {
-                int t = x;
-                x = y;
-                y = t;
+    }
+
+    public int[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
+        int[][] arr = new int[n][2]; 
+        for(int i = 0; i < n; i++){
+            arr[i][0] = nums[i];
+            arr[i][1] = i;
+        }
+
+        Arrays.sort(arr, (a, b) -> {
+            return Integer.compare(a[0], b[0]);
+        });
+
+        int[] NodeToIdx = new int[n];
+        for(int i = 0; i < n; i++){
+            int node = arr[i][1];
+            NodeToIdx[node] = i; 
+        }
+        double result = Math.log(n) / Math.log(2);
+        int cols = (int)result + 1; 
+
+        int rows = n; 
+        int[][] up = new int[rows][cols]; 
+
+        // furthest we can go in 1 jump from node
+        for(int node = 0; node < n; node++){
+            int furtherst = UpperBoundBinary(arr, arr[node][0] + maxDiff);
+            up[node][0] = furtherst;
+        } 
+
+        for(int j = 1; j < cols; j++){
+            for(int node = 0; node < n; node++){
+                up[node][j] = up[up[node][j - 1]][j - 1];
             }
+        }
+        int qs = queries.length;
+        int[] res = new int[qs];
+        int i = 0;
+        for(int[] q : queries){
+            int u = q[0];
+            int v = q[1];
 
-            if (x == y) {
-                ans[k] = 0;
-                continue;
+            int a = NodeToIdx[u];
+            int b = NodeToIdx[v];
+            if(a == b){
+                res[i] = 0; 
+                i++;
+                continue; 
             }
-
-            int step = 0;
-
-            for (int i = m - 1; i >= 0; i--) {
-                if (f[y][i] > x) {
-                    y = f[y][i];
-                    step += 1 << i;
+            if(a > b){
+                int temp = b; 
+                b = a; 
+                a = temp; 
+            }
+            int curr = a; 
+            int jump = 0; 
+            for(int col = cols - 1; col >= 0; col--){
+                if(up[curr][col] < b){
+                    curr = up[curr][col];
+                    jump += (1 << col);
                 }
             }
 
-            ans[k] = f[y][0] <= x ? step + 1 : -1;
+            if(up[curr][0] >= b){
+                res[i] = jump + 1;
+            } else {
+                res[i] = -1; 
+            }        
+            i++;
         }
 
-        return ans;
+        return res; 
+
     }
 }
