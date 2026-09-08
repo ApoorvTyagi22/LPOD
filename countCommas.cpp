021c67885f80c251749581bcff32ce1965809279
Apoorv Tyagi <at2u23@soton.ac.uk>
2026-09-08 23:26:49 +0100

    Tue8Sep

---
+++ countCommas.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        int res = 0; 
+        for(int i= 1; i <= n; i++){
+            if(i > 999){
+                res++;
+            }
+        }
+
+        return res; 
+    }
+};
