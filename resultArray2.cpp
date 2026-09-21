ca211b108fe68270fb2397f3e60e9a7f83d6aef0
Apoorv Tyagi <at2u23@soton.ac.uk>
2026-09-21 23:12:11 +0100

    Mon21Sep

---
+++ resultArray2.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<long long> resultArray(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        vector<long long> result(k, 0); 
+
+        vector<long long> prevCount(k, 0);
+
+        for(int i = 0; i < n; i++){
+            vector<long long> currCnt(k, 0);
+
+            int currElemenRem = nums[i] % k; 
+
+            currCnt[currElemenRem]++;
+
+            for(int oldRem = 0; oldRem < k; oldRem++){
+                int newRem = ((long long)oldRem * nums[i] % k) % k; 
+                currCnt[newRem] += prevCount[oldRem];
+            }
+
+            prevCount = move(currCnt);
+
+            for(int x = 0; x <= k - 1; x++){
+                result[x] += prevCount[x];
+            }            
+        }
+
+
+        return result;
+
+
+
+
+    }
+};
